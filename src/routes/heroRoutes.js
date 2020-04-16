654aaa07362ad72def6a7c23b2f68ea2a6ab00c6
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2020-04-16 11:29:49 -0300

    Aula 14
    Implementaçao do método Listar()
    Desenvolvimento orientado a testes

--- src/routes/heroRoutes.js
+++ src/routes/heroRoutes.js
@@ -1,3 +1,4 @@
+const boom = require("boom");
 const BaseRoutes = require("./base/baseRoute");
 
 class HeroRoutes extends BaseRoutes {
@@ -8,10 +9,37 @@ class HeroRoutes extends BaseRoutes {
 
   list() {
     return {
-      path: "/heroes",
+      path: "/herois",
       method: "GET",
       handler: (request, headers) => {
-        return this.db.read();
+        try {
+          const { nome, skip, limit } = request.query;
+
+          let query = {};
+          if (nome) {
+            query.nome = nome;
+          }
+          if (isNaN(limit) && limit !== undefined)
+            throw Error("LIMITE-INVALIDO");
+          if (isNaN(skip) && skip !== undefined) throw Error("PULAR-INVALIDO");
+
+          return this.db.read(query, parseInt(skip), parseInt(limit));
+        } catch (error) {
+          console.log("Deu RUIM", error);
+          switch (error.message) {
+            case "LIMITE-INVALIDO":
+              throw boom.badRequest(
+                "O valor informado para o limite não é um número válido"
+              );
+            case "PULAR-INVALIDO":
+              throw boom.badRequest(
+                "O valor informado para o skip não é um número válido"
+              );
+            default:
+              throw boom.badImplementation(error);
+          }
+          return headers.response("Erro interno no servidor").code(500);
+        }
       },
     };
   }
--- src/tests/apiHeroes.test.js
+++ src/tests/apiHeroes.test.js
@@ -2,20 +2,55 @@ const assert = require("assert");
 const api = require("../api");
 let app = {};
 
-describe("## Suíte de testes da API Heroes", function () {
+describe.only("## Suíte de testes da API Heroes", function () {
   this.beforeAll(async () => {
     app = await api;
   });
+
   it("Deverá listar os heróis cadastrados", async () => {
     const result = await app.inject({
       method: "GET",
-      url: "/heroes",
+      url: "/herois",
     });
 
     const dados = JSON.parse(result.payload);
     const statusCode = result.statusCode;
     assert.deepEqual(statusCode, 200);
     assert.ok(Array.isArray(dados));
-    console.log(result);
+  });
+
+  it("Deverá retornar uma lista reduzida de heróis utilizando filtros (no máx 10 itens)", async () => {
+    const LIMITE = 10;
+    const result = await app.inject({
+      method: "GET",
+      url: `/herois?skip=0&limit=${LIMITE}`,
+    });
+
+    const dados = JSON.parse(result.payload);
+    const statusCode = result.statusCode;
+    assert.deepEqual(statusCode, 200);
+    assert.ok(dados.length <= LIMITE);
+  });
+
+  it("Não deverá retornar heróis por erro interno", async () => {
+    const LIMITE = "AAA";
+    const result = await app.inject({
+      method: "GET",
+      url: `/herois?skip=0&limit=${LIMITE}`,
+    });
+    const statusCode = result.statusCode;
+    assert.deepEqual(statusCode, 400);
+  });
+
+  it("Deverá retornar Heróis que contenham o nome Gavião Negro", async () => {
+    const nome = "Gavião Negro";
+    const result = await app.inject({
+      method: "GET",
+      url: `/herois?skip=0&limit=10&nome=${nome}`,
+    });
+    const dados = JSON.parse(result.payload);
+    const statusCode = result.statusCode;
+    assert.deepEqual(statusCode, 200);
+    assert.ok(dados.length >= 1);
   });
 });
